9273fe9ef3d9d4eae2c168fca108680eb653860f
Soumajit Choudhuri <43232595+Soumajit2000@users.noreply.github.com>
2020-07-11 14:14:14 +0530

    adding back end code

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs'); 
+const cors = require('cors');
+const knex = require('knex');
+
+const app = express();
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl : true
+  }
+});  
+
+
+app.use(cors());
+app.use(express.json());
+
+//ROOT END POINT
+app.get('/', (req, res) => { res.send('IT IS WORKING!!') })
+
+//SIGNING IN INTO THE ACCOUNT
+app.post('/signin', signin.handleSignin(db, bcrypt))
+//REGISTERING NEW USER
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+//INCREASING THE ENTRIES AGAINST EACH SEARCH
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+
+//FINDING DETAILS ABOUT A PARTICULAR USER
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+//HANDLING THE API CALL FROM CLARIFAI
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 3001, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`)
+})
